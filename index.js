78d42cd077851e545593b31d6360a668543a64b0
gaby magnussen <gabymagnussen@gmail.com>
2020-02-23 16:48:30 -0600

    added js file

---
+++ index.js
@@ -0,0 +1,28 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+inquirer
+    .prompt([
+        {
+            name: 'username',
+            message: 'What is your GitHub username?',
+            type: 'input',
+        },
+        {
+            name: 'projectTitle',
+            message: 'What is the project called?',
+            type: 'input',
+        }
+        {
+            name: 'projectDescription',
+            message: 'What is the project about?',
+            type: 'input',
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+        let file = `
+        # Title
+        The user first name is ${answers.firstname}
+        `
+        fs.writeFile('README.md', file, () => {})
+    });
