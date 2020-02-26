a14d8fded39a44add405e86df49b2dcfd526262e
gaby magnussen <gabymagnussen@gmail.com>
2020-02-26 17:44:17 -0600

    added node stuff

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
-const inquirer = require('inquirer');
+const axios = require('axios');
 const fs = require('fs');
+const inquirer = require('inquirer');
 inquirer
-    .prompt([
-        {
-            name: 'username',
-            message: 'What is your GitHub username?',
-            type: 'input',
-        },
-        {
-            name: 'projectTitle',
-            message: 'What is the project called?',
-            type: 'input',
+  .prompt([
+    {
+      name: 'github',
+      message: 'What is your github username?',
+      type: 'input',
+    },{
+        name: 'projectTitle',
+        message: 'What is the project called?',
+        type: 'input',
+                
+    },
+    {
+        name: 'projectDescription',
+        message: 'What is the project about?',
+        type: 'input',
+                }
+  ])
+  .then(answers => {
+    let username = answers.github;
+    axios.get('https://api.github.com/users/' + username)
+      .then(function (response) {
+        let content = '';
+        for (const key in response.data) {
+          content += key + ': ' + response.data[key] + '\n\n';
         }
-        {
-            name: 'projectDescription',
-            message: 'What is the project about?',
-            type: 'input',
-        }
-    ])
-    .then(answers => {
-        console.log(answers);
-        let file = `
-        # Title
-        Project Title ${answers.projectTitle}
-        `
-        fs.writeFile('README.md', file, () => {})
-    });
+        fs.writeFile('readme.md', content, function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+        }); 
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+
+//   const inquirer = require('inquirer');
+
+
+// const fs = require('fs');
+// inquirer
+//     .prompt([
+//         {
+//             name: 'username',
+//             message: 'What is your GitHub username?',
+//             type: 'input',
+//         },
+//     
+//        
+//     ])
+//     .then(answers => {
+//         console.log(answers);
+//         let file = `
+//         # Title
+//         Project Title ${answers.projectTitle}
+//         `
+//         fs.writeFile('README.md', file, () => {})
+//     });
---
+++ readme.md
@@ -0,0 +1,63 @@
+login: gabymag
+
+id: 58097964
+
+node_id: MDQ6VXNlcjU4MDk3OTY0
+
+avatar_url: https://avatars1.githubusercontent.com/u/58097964?v=4
+
+gravatar_id: 
+
+url: https://api.github.com/users/gabymag
+
+html_url: https://github.com/gabymag
+
+followers_url: https://api.github.com/users/gabymag/followers
+
+following_url: https://api.github.com/users/gabymag/following{/other_user}
+
+gists_url: https://api.github.com/users/gabymag/gists{/gist_id}
+
+starred_url: https://api.github.com/users/gabymag/starred{/owner}{/repo}
+
+subscriptions_url: https://api.github.com/users/gabymag/subscriptions
+
+organizations_url: https://api.github.com/users/gabymag/orgs
+
+repos_url: https://api.github.com/users/gabymag/repos
+
+events_url: https://api.github.com/users/gabymag/events{/privacy}
+
+received_events_url: https://api.github.com/users/gabymag/received_events
+
+type: User
+
+site_admin: false
+
+name: Gaby Magnussen
+
+company: null
+
+blog: https://gabymag.github.io/portfolio/
+
+location: Dallas, TX
+
+email: null
+
+hireable: true
+
+bio: Currently working on a full stack web development certification through Trilogy Education at Southern Methodist University.
+
+
+public_repos: 9
+
+public_gists: 0
+
+followers: 6
+
+following: 7
+
+created_at: 2019-11-23T03:28:58Z
+
+updated_at: 2020-02-23T22:48:43Z
+
